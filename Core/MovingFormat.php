bbbcb7d408b4b2bb89ab71a237e75120b66cab9c
Dominik Klapuch <klapuchdominik@gmail.com>
2017-10-04 19:35:09 +0200

    Added MovingFormat

---
+++ Core/MovingFormat.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types = 1);
+namespace Klapuch\Output;
+
+/**
+ * Format moving keys by your choice
+ */
+final class MovingFormat implements Format {
+	private $origin;
+	private $source;
+	private $moves;
+
+	public function __construct(Format $origin, array $source, array $moves) {
+		$this->origin = $origin;
+		$this->source = $source;
+		$this->moves = $moves;
+	}
+
+	public function with($tag, $content = null): Format {
+		return $this->origin->with($tag, $content);
+	}
+
+	public function serialization(): string {
+		return (new FilledFormat(
+			$this->origin,
+			$this->copy($this->moves($this->source, $this->moves), $this->source)
+		))->serialization();
+	}
+
+	public function adjusted($tag, callable $adjustment): Format {
+		return $this->origin->adjusted($tag, $adjustment);
+	}
+
+	/**
+	 * Copy moves to source
+	 * @param array $moves
+	 * @param array $source
+	 * @return array
+	 */
+	private function copy(array $moves, array $source): array {
+		return array_reduce(
+			array_keys($moves),
+			function(array $copies, $field) use ($source, $moves): array {
+				if (is_array($moves[$field]))
+					$copies[$field] = $this->copy($moves[$field], $source);
+				else $copies[$moves[$field]] = $source[$moves[$field]];
+				return $copies;
+			},
+			[]
+		);
+	}
+
+	private function moves(array $source, array $moves): array {
+		return array_filter(
+			array_replace_recursive($source, $moves),
+			function($key) use ($moves): bool {
+				return is_int($key) || is_array($moves[$key] ?? null);
+			},
+			ARRAY_FILTER_USE_KEY
+		);
+	}
+}
---
+++ Tests/Unit/MovingFormat.phpt
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+/**
+ * @testCase
+ * @phpVersion > 7.1
+ */
+namespace Klapuch\Output\Unit;
+
+use Klapuch\Output;
+use Tester\Assert;
+
+require __DIR__ . '/../bootstrap.php';
+
+final class MovingFormat extends \Tester\TestCase {
+	public function testMovingByKeys() {
+		Assert::same(
+			'<root><general><age>21</age><firstname>Dom</firstname><hair><color>blue</color></hair></general><id>1</id><seeker_id>2</seeker_id></root>',
+			(new Output\MovingFormat(
+				new Output\Xml([], 'root'),
+				['age' => 21, 'firstname' => 'Dom', 'id' => 1, 'seeker_id' => 2, 'color' => 'blue'],
+				['general' => ['age', 'firstname', 'hair' => ['color']], 'id', 'seeker_id']
+			))->serialization()
+		);
+	}
+}
+
+(new MovingFormat())->run();
